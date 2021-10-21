ec781b9b457f857a4af2d7ea2ef83e58221dc06c
jakobmaier-eu <jakobmaier@gmx.at>
2021-10-21 09:40:44 +0200

    commented code in street aggregation data load file

--- step1_load_data.R
+++ step1_load_data.R
@@ -11,23 +11,13 @@ library(ranger)
 #----------- End Header ----------------#
 
 
-#----------- Loading data from all years into data frames ----#
-# Too heavy calculation so far!
-
-# years = c(2014, 2015, 2016, 2017, 2018, 2019, 2020)
-# no_years = 7
-# years = c(2014)
-# no_years = 1
-# data_raw = vector(mode="list", length=no_years)
-# names(data_raw) = years
-# for (year in years){
-# }
   
-#------ Reading data week by week ---------#
+#------ Reading data week by week ---------# EXECUTED AND SAVED
+
 
 
 years = c(2014, 2015, 2016, 2017, 2018, 2019, 2020)
-years = c(2020)
+# years = c(2020)
 for (year in years){
   foldername = paste("data/data_raw_", year, sep="")
   filenames = list.files(foldername)
@@ -46,22 +36,24 @@ for (year in years){
 }
 
 
+#### We don't remember whether we've used this code.
+# all_main_streets = data.frame()
+# foldername = "data/find_main_streets/"
+# filenames = list.files(foldername)
+# for (filename in filenames){
+#   filepath = paste(foldername, "/", filename, sep = "")
+#   week_df = read_delim(filepath, col_names =TRUE, delim=';')
+#   agg_streets = aggregate(week_df$q, by=list(week_df$libelle), FUN=mean, na.rm = TRUE)
+#   names(agg_streets) = c("libelle", "q")
+#   agg_streets_no_periph = agg_streets[!(str_sub(agg_streets$libelle, 1,2) == "PE" 
+#                                         | str_sub(agg_streets$libelle, 1,2)== "PI"),]
+#   main_streets = agg_streets_no_periph[order(agg_streets_no_periph$q, decreasing = T),]
+#   saveRDS(main_streets, file=paste("Data/find_main_streets/", year, "_week_",i,".rds", sep = ""))
+# }
+# 
+# 
+# w1_2014 = readRDS("data/find_main_streets/2014_week_0.rds")
 
-all_main_streets = data.frame()
-foldername = "data/find_main_streets/"
-filenames = list.files(foldername)
-for (filename in filenames){
-  filepath = paste(foldername, "/", filename, sep = "")
-  week_df = read_delim(filepath, col_names =TRUE, delim=';')
-  agg_streets = aggregate(week_df$q, by=list(week_df$libelle), FUN=mean, na.rm = TRUE)
-  names(agg_streets) = c("libelle", "q")
-  agg_streets_no_periph = agg_streets[!(str_sub(agg_streets$libelle, 1,2) == "PE" 
-                                        | str_sub(agg_streets$libelle, 1,2)== "PI"),]
-  main_streets = agg_streets_no_periph[order(agg_streets_no_periph$q, decreasing = T),]
-  saveRDS(main_streets, file=paste("Data/find_main_streets/", year, "_week_",i,".rds", sep = ""))
-}
-
-
-w1_2014 = readRDS("data/find_main_streets/2014_week_0.rds")
+#-------------------------------------------------#
 
 
