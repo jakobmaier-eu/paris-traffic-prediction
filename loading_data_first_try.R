7255fa0fd350be3fa6412f86e09630a6bde24f32
Guillaume Lambert <lambert_guillaume@outlook.fr>
2021-11-04 18:24:32 +0100

    periph

--- loading_data_first_try.R
+++ loading_data_first_try.R
@@ -145,8 +145,134 @@ str
 
 writeClipboard(str)
 
+########
 
+edges_name <- read.delim("liste_aretes.txt", header = F)$V1
 
+#pont amont - pont austerlitz
+c("Quai_de_Bercy_W","Quai_Bercy_Rapee_W")
 
+#pont austerlitz - pont amont
+c("Quai_Rapee_Bercy_Y","Quai_de_Bercy_Y","Quai_de_la_Rapee_Y")
+
+#saint michel - concorde
+c("Quai_Anatole_France","Quai_Conti","Quai_Malaquais","Quai_Voltaire","Quai_Grands_Augustins")
+
+#concorde - chatelet
+c("VGP_Quai_des_Tuileries","Quai_du_Louvre","Quai_de_la_Megisserie","Quai_Tuileries","Quai_des_Tuileries","VGP_Souterrain_Tuileries","VGP_Quai_Tuileries","Quai_du_Louvre","VGP_Quai_des_Tuileries")
+
+#saint michel - chatelet
+c("Bd_du_Palais","Pt_au_Change","Pt_St_Michel")
+
+#rond point etoile - porte maillot
+c("Av_Grande_Armee")
+
+#chatelet - pont austerlitz
+c("Quai_de_la_Rapee_W","V.G.P","Quai_Henri_IV","VGP_Souterrain_Henri_IV")
+
+#chatelet - saint michel
+c("Petit_Pont","Pt_Notre_Dame","Cite")
+
+#jussieu - bastille
+c("Pt_Sully","Bd_Henri_IV")
+
+#strasbourg saint denis - porte de la chapelle
+c("Bd_de_Stasbourg","Bd_de_Strasbourg")
+
+#porte maillot - rond point etoile
+c("Av_de_la_Grande_Armee")
+
+#jussieu - saint michel
+c("Quai_Tournelle","Quai_Montebello","Quai_St_Michel")
+
+#strasbourg saint denis - chatelet
+c("Renard","Beaubourg")
+
+#pont alma - concorde
+c("Souterrain_Invalides_VGP")
+
+#concorde - saint michel
+c("Bd_St_Germain")
+
+#bastille - chatelet
+c("St_Antoine")
+
+#pont austerlitz - chatelet
+c("Bd_Morland")
+
+#saint lazare - strasbourg saint denis
+c("4_Septembre","Pl_de_la_Bourse_cote_Reaumur")
+
+#chatelet - strasboug saint denis
+c("Bd_Sebastopol")
+
+#porte orleans - denfert rochereau
+c("Pte_d'Orleans")
+
+#chatelet - concorde
+c("Rivoli")
+
+#bastille - pont austerlitz 
+c("Bd_Bourdon")
+
+#pont aval - pont alma
+c("Quai_St_Exupery")
+
+#concorde - pont alma
+c("Cours Albert 1er_VGP")
+
+#pont austerlitz - bastille
+c("Bd_Bastille")
+
+#round point etoile - pont alma
+c("Av_Marceau")
+
+#pont aval - porte orleans
+c("PE_Parc_Expo_Ouest","PE_Plaine","PE_Chatillon")
+
+#porte orleans - pont aval
+c("PI_Parc_Expo_Ouest","PI_Plaine","PI_Chatillon")
+
+#porte orleans - porte italie
+c("PE_Gentilly")
+
+#porte italie - porte orleans
+c("PI_Gentilly")
+
+#porte italie - pont amont
+c("PE_Haubans")
+
+#pont amont - porte italie
+c("PI_Haubans")
+
+#pont amont - porte vincennes
+c("PE_Reuilly")
+
+#porte vincennes - pont amont
+c("PI_Reuilly")
+
+#porte vincennes - bagnolet
+c("PE_Louis_Lumiere")
+
+#bagnolet - porte vincennes
+c("PI_Louis_Lumiere")
+
+#bagnolet - porte de la chapelle
+c("PE_Chaumont","PE_Aubervilliers")
+
+#porte de la chapelle - bagnolet
+c("PI_Chaumont","PI_Aubervilliers")
+
+#porte de la chapelle - porte asnieres
+c("PE_Batignolles","PE_Poissonniers")
+
+#porte asnieres - porte de la chapelle
+c("PI_Batignolles","PI_Poissonniers")
+
+#porte asnieres - porte maillot
+c("PE_Champerret")
+
+#porte maillot - porte asnieres
+c("PI_Champerret")
 
 
