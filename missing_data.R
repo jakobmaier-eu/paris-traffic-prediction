0dcea4d02d19c059d407bc1d7e8ea6b3970e704e
jakobmaier-eu <jakobmaier@gmx.at>
2021-11-12 14:05:29 +0100

    missing data file created - forgot to save

--- missing_data.R
+++ missing_data.R
@@ -10,4 +10,20 @@ library(lubridate)
 library(weathermetrics)
 library(ranger)
 
-df = readRDS("Data/data_agg69_plain/edges_dfs_allyrs.rds")
+edges = readRDS("Data/data_agg69_plain/edges_dfs_allyrs.rds")
+
+
+# Counting NAs. Some q-counters (number of cars passing) have 100% missing data. Need to find the counting points with capteurs. 
+for (i in 1:length(edges)){
+  print(colSums(is.na(edges[[i]]))/nrow(edges[[i]]))
+}
+
+for (i in 1:length(edges)){
+  if(sum(is.na(edges[[i]]$q))/nrow(edges[[i]]) > 0.1){
+    print(names(edges)[i])
+  }
+}
+
+
+
+
