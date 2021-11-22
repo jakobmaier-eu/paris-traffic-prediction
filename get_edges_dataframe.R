e9ed2e0bf0047f047f7464709ac0b39f735b8d41
jakobmaier-eu <jakobmaier@gmx.at>
2021-11-22 18:03:21 +0100

    Considerations on misssing data

--- get_edges_dataframe.R
+++ get_edges_dataframe.R
@@ -469,7 +469,7 @@ proc.time() - ptm
 for (j in 1:length(edges_dfs_allyrs)){
   print(names(edges_dfs_allyrs)[j])
   edges_dfs_allyrs[[j]] = aggregate(edges_dfs_allyrs[[j]], 
-                 by = list(elem$t_1h), FUN = mean, na.rm = TRUE)
+                 by = list(edges_dfs_allyrs[[j]]$t_1h), FUN = mean, na.rm = TRUE)
 }
 
 saveRDS(edges_dfs_allyrs, "Data/data_agg69_plain/edges_dfs_allyrs.rds")
