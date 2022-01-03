0543677b0effa82d25306ebb4ae0d58b6170b943
jakobmaier-eu <jakobmaier@gmx.at>
2022-01-03 18:43:09 +0100

    train test split and modified imputation file

--- missing_data_Jakob.R
+++ missing_data_Jakob.R
@@ -35,50 +35,81 @@ image(
 #-------- Filling NAs with Random Forest: EXECUTED ----------------
 
 # dfs = readRDS("Data/edges_with_neigh.rds")
+# 
 # library(missRanger)
 # library(caret)
 # library(visdat)
 # library(miceRanger)
+# library(dplyr)
 # 
-# imp_dfs = vector("list", length=length(dfs))
-# imp_VarImportance = vector("list", length=length(dfs))
-# missing_percent = vector("list", length=length(dfs))
-# names(imp_dfs) = names(dfs)
-# names(imp_VarImportance) = names(dfs)
-# names(missing_percent) = names(dfs)
+# imp_edges_train = vector("list", length=length(dfs))
+# imp_VarImportance_train = vector("list", length=length(dfs))
+# missing_percent_train = vector("list", length=length(dfs))
+# names(imp_edges_train) = names(dfs)
+# names(imp_VarImportance_train) = names(dfs)
+# names(missing_percent_train) = names(dfs)
 # 
-# 
-# 
-# start_time = Sys.time()
-# for (i in 1:length(dfs)){
-#   df = dfs[[i]]
-#   edgename = names(dfs)[i]
-#   varnames = names(df)
-#   missing_percent[[i]] = c(round(sum(is.na(df$rateCar))/length(df$rateCar), 4)*100,
-#                            round(sum(is.na(df$nbCar))/length(df$nbCar), 4)*100)
-#   vars <- list(
-#     rateCar = varnames[! varnames %in% c('iu_ac', 't_1h', 'covidIndex', 'rateCar')],
-#     nbCar = varnames[! varnames %in% c('iu_ac', 't_1h', 'covidIndex', 'nbCar')]
-#   )
-#   mice_obj <- miceRanger(
-#     df,
-#     m = 1,
-#     maxiter = 5,
-#     vars = vars,
-#     verbose=TRUE,
-#     num.trees = 100,
-#     mtry = 7
-#   )
-#   imp = completeData(mice_obj)[[1]]
-#   imp_VarImportance[[i]] = mice_obj$finalImport[[1]]
-#   imp_dfs[[i]] = imp
+# ### Fix some type issues:
+# for (i in 1:69){
+#   dfs[[i]] = dfs[[i]][-(1:2)]
+#   for (j in 4:7){
+#     dfs[[i]][j] = list(as.integer(unlist(dfs[[i]][j])))
+#   }
 # }
-# end_time = Sys.time()
-# time_elapsed = end_time - start_time
 # 
-# saveRDS(imp_dfs, "Data/imputed_edges_neigh.rds")
-# saveRDS(imp_VarImportance, "Data/imputation_VarImportance.rds")
-# saveRDS(missing_percent, "Data/imputation_missing_percent.rds")
+# ### Train-Test-Split:
+# edges_test = vector("list", length=length(dfs))
+# names(edges_test) = names(dfs)
+# edges_train = vector("list", length=length(dfs))
+# names(edges_train) = names(dfs)
+# edges_2020 = vector("list", length=length(dfs))
+# names(edges_2020) = names(dfs)
+# 
+# for (i in 1:length(edges_test)){
+#   edges_test[[i]] = filter(dfs[[i]], year %in% c(2018, 2019))
+#   edges_train[[i]] = filter(dfs[[i]], year %in% c(2014, 2015, 2016, 2017))
+#   edges_2020[[i]] = filter(dfs[[i]], year == 2020)
+# }
+# 
+# saveRDS(edges_test, "Data/edges_test.rds")
+# saveRDS(edges_train, "Data/edges_train.rds")
+# saveRDS(edges_test, "Data/edges_2020.rds")
+
+edges_test = readRDS("Data/edges_test.rds")
+edges_train = readRDS("Data/edges_train.rds")
+edges_2020 = readRDS("Data/edges_2020.rds")
+
+
+start_time = Sys.time()
+for (i in 1:length(edges_train)){
+  df = edges_train[[i]]
+  edgename = names(edges_train)[i]
+  varnames = names(df)
+  missing_percent_train[[i]] = c(round(sum(is.na(df$rateCar))/length(df$rateCar), 4)*100,
+                           round(sum(is.na(df$nbCar))/length(df$nbCar), 4)*100)
+  vars <- list(
+    rateCar = varnames[! varnames %in% c('covidIndex', 'rateCar')],
+    nbCar = varnames[! varnames %in% c('covidIndex', 'nbCar')]
+  )
+  mice_obj <- miceRanger(
+    df,
+    m = 1,
+    maxiter = 5,
+    vars = vars,
+    verbose=TRUE,
+    num.trees = 100,
+    mtry = 7
+  )
+  imp = completeData(mice_obj)[[1]]
+  imp_VarImportance_train[[i]] = mice_obj$finalImport[[1]]
+  imp_edges_train[[i]] = imp
+}
+end_time = Sys.time()
+time_elapsed = end_time - start_time
+
+saveRDS(imp_edges_train, "Data/imp_edges_train.rds")
+saveRDS(imp_VarImportance_train, "Data/imp_VarImportance_train.rds")
+saveRDS(missing_percent_train, "Data/imp_missing_percent_train.rds")
 
 
 #### NOT YET EXECUTED:
