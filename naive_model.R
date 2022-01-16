ed25614e3cd8f4da889dce8583afc408f4b93cf1
Guillaume Lambert <lambert_guillaume@outlook.fr>
2022-01-16 20:53:48 +0100

    RF

--- naive_model.R
+++ naive_model.R
@@ -1,49 +1,30 @@
 rm(list=objects())
 
+#Libraries
 library(lubridate)
 library(dplyr)
+library(ProjetML1)
 
+#Data sets
 data_train = readRDS("data/imp_edges_train.rds")
 data_test <- readRDS("data/imp_edges_test.rds")
 
-F1 <- function(nb, h){
-  return(mean((data_train[[nb]] %>% filter(hour == h))$rateCar))
-}
-
-F2 <- function(nb, h){
-  return(median((data_train[[nb]] %>% filter(hour == h))$rateCar))
-}
-
-F3 <- function(nb, h, d, m){
+#Naive functions
+F1 <- function(nb, h, d, m){
   return(mean((data_train[[nb]] %>% filter(hour == h, day == d, month == m))$rateCar))
 }
 
-F4 <- function(nb, h, d, m){
+F2 <- function(nb, h, d, m){
   return(median((data_train[[nb]] %>% filter(hour == h, day == d, month == m))$rateCar))
 }
 
-# naiveModel <- function(FUN){
-#   pred_list = vector("list", length=69)
-#   for(i in 1:69){
-#     pred_elt <- c()
-#     
-#     for(j in 0:23){
-#       temp <- FUN(i, j)
-#       pred_elt <- c(pred_elt, temp)
-#     }
-#     
-#     pred_list[[i]] <- pred_elt
-#   }
-#   
-#   return(pred_list)
-# }
-
-naiveModel2 <- function(FUN){
+#Naive Model
+naiveModel <- function(FUN){
   pred_list = vector("list", length=69)
   for(i in 1:69){
     pred_elt <- c()
     
-    print(i)
+    # print(i) to check
 
     for(j in 1:dim(data_test[[1]])[1]){
       temp <- FUN(i, 
@@ -59,30 +40,52 @@ naiveModel2 <- function(FUN){
   return(pred_list)
 }
 
+#Naive predictions (1 and 2)
+naivePrediction1 <- naiveModel(F1)
+naivePrediction2 <- naiveModel(F2)
 
-
-# naivePrediction1 <- naiveModel(F1)
-# naivePrediction2 <- naiveModel(F2)
-# 
 # saveRDS("data/naivePrediction1.rds", object = naivePrediction1)
 # saveRDS("data/naivePrediction2.rds", object = naivePrediction2)
 
-# naivePrediction3 <- naiveModel2(F3)
+# Naive Model bis
+naiveModelBis <- function(){
+  pred_list = vector("list", length=69)
+  for(i in 1:69){
+    pred_list[[i]] <- data_test[[i]]$rateCar_LaggedHour
+  }
+  return(pred_list)
+}
+
+# Naive Prediction (3)
+naivePrediction3 <- naiveModelBis(F3)
 # saveRDS("data/naivePrediction3.rds", object = naivePrediction3)
 
-naivePrediction4 <- naiveModel2(F4)
-# saveRDS("data/naivePrediction4.rds", object = naivePrediction4)
+############################################
+############################################
+############################################
 
-# naiveModel3 <- function(){
-#   pred_list = vector("list", length=69)
-#   for(i in 1:69){
-# 
-#     
-#     pred_list[[i]] <- data_test[[i]]$rateCar_LaggedHour
-#   }
-#   
-#   return(pred_list)
-# }
+# Load naive predictions
+pred1 = readRDS("data/naivePrediction1.rds")
+pred2 = readRDS("data/naivePrediction2.rds")
+pred3 = readRDS("data/naivePrediction3.rds")
 
-naivePrediction5 <- naiveModel3()
-# saveRDS("data/naivePrediction5.rds", object = naivePrediction5)
+# rmse and mape 
+rmse_list_naivePrediction3 = c()
+mape_list_naivePrediction3 = c()
+
+for(i in 1:69){
+  rmse_list_naivePrediction3 <- c(rmse_list_naivePrediction3, rmse(pred3[[i]], data_test[[i]]$rateCar))
+  mape_list_naivePrediction3 <- c(mape_list_naivePrediction3, mape(pred3[[i]], data_test[[i]]$rateCar))
+}
+
+# save(rmse_list_naivePrediction2, mape_list_naivePrediction2, file = "naivePrediction2.RData")
+
+
+
+
+
+
+
+
+
+
--- utilisationRF.R
+++ utilisationRF.R
@@ -5,21 +5,26 @@ library("vip")
 library("tuneRanger")
 library("mlr")
 library("caret")
+library(ProjetML1)
 
-rmse<-function(eps)
-{
-  return(round(sqrt(mean(eps^2,na.rm=TRUE)),digits=2))
-}
-
-mape<-function(y,ychap)
-{
-  return(round(100*mean(abs(y-ychap)/abs(y)),digits=2))
-}
+# rmse<-function(eps)
+# {
+#   return(round(sqrt(mean(eps^2,na.rm=TRUE)),digits=2))
+# }
+# 
+# mape<-function(y,ychap)
+# {
+#   return(round(100*mean(abs(y-ychap)/abs(y)),digits=2))
+# }
 
 set.seed(1)
 
 edges = readRDS("data/imp_edges_train.rds")
 edges_test = readRDS("data/imp_edges_test.rds")
+
+###############################################
+##### TEST
+###############################################
 
 res <- ranger(rateCar ~ ., 
                    data=edges[[1]], 
@@ -31,7 +36,13 @@ summary(res)
 
 res$variable.importance[order(res$variable.importance)]
 
-###
+############################################
+############################################
+############################################
+
+############################################
+##### TUNING
+############################################
 
 data = edges[[1]][,-1]
 data_test = edges_test[[1]][,-1]
@@ -48,8 +59,8 @@ data_test$hour <- as.factor(data_test$hour)
 
 ptm <- proc.time()
 
-grid <-  expand.grid(mtry = seq(2,34,by=2), 
-                     min.node.size = seq(5,1000,by=200),
+grid <-  expand.grid(mtry = 5, 
+                     min.node.size = seq(1,50,by=1),
                      splitrule = "variance")
 
 fitControl <- trainControl(method = "timeSlice",
@@ -63,14 +74,16 @@ fit = caret::train(
   x = data[,-2],
   y = data$rateCar,
   method = 'ranger',
-  num.trees = 50,
+  num.trees = 100,
   tuneGrid = grid,
   trControl = fitControl)
 
 print(fit)
 print(proc.time() - ptm) 
 
-#######
+############################################
+############################################
+############################################
 
 model1 <- ranger(rateCar ~ ., 
                data=data, 
@@ -112,16 +125,36 @@ plot(1:1000,prediction2$predictions[1:1000], type = 'l')
 rmse_list = c()
 mape_list = c()
 
+ptm <- proc.time()
+
 for(i in 1:69){
+  print(i)
+  
+  data = edges[[i]][,-1][,c(1:22)]
+  data_test = edges_test[[i]][,-1][,c(1:22)]
+  
+  data$day <- as.factor(data$day)
+  data$year <- as.factor(data$year)
+  data$month <- as.factor(data$month)
+  data$hour <- as.factor(data$hour)
+  
+  data_test$day <- as.factor(data_test$day)
+  data_test$year <- as.factor(data_test$year)
+  data_test$month <- as.factor(data_test$month)
+  data_test$hour <- as.factor(data_test$hour)
+  
   model <- ranger(rateCar ~ ., 
-                   data=data_train, 
+                   data=data, 
+                  num.trees = 300,
                    importance='impurity')
   
   prediction <- predict(model, data=data_test)
   
-  rmse_list <- c(rmse_list, rmse(prediction$predictions-data_test$rateCar))
-  mape_list <- c(mape_list, rmse(prediction$predictions, data_test$rateCar))
+  rmse_list <- c(rmse_list, rmse(prediction$predictions, data_test$rateCar))
+  mape_list <- c(mape_list, mape(prediction$predictions, data_test$rateCar))
 }
+
+print(proc.time() - ptm) 
 
 #######
 
@@ -145,6 +178,51 @@ for(i in 1:69){
 #                    splitrule = "variance")
 # 
 # print(rf_random)
+
+##############################################
+##############################################
+##############################################
+
+error_list <- c()
+
+ntree_list <- c(5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
+
+time_list <- c()
+
+for(n in ntree_list){
+  print(n)
+  
+  ptm <- proc.time()
+  
+  res <- ranger(rateCar ~ ., 
+                data=edges[[1]], 
+                num.trees = n,
+                importance='impurity')
+  
+  temp <- proc.time() - ptm
+  
+  time_list <- c(time_list, temp[3])
+  
+  error_list <- c(error_list, res$prediction.error)
+}
+
+plot(ntree_list, error_list)
+plot(ntree_list, time_list)
+
+
+
+
+
+ptm <- proc.time()
+
+test <- proc.time() - ptm
+
+
+
+
+
+
+
 
 
 
