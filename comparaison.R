100e80b7de15ba50d8bb39fef4dc3c2c68a5e602
Guillaume Lambert <lambert_guillaume@outlook.fr>
2022-01-18 23:12:06 +0100

    rédaction

--- comparaison.R
+++ comparaison.R
@@ -1,19 +1,22 @@
 rm(list=objects())
 
-rmse<-function(eps)
-{
-  return(round(sqrt(mean(eps^2,na.rm=TRUE)),digits=0))
-}
+library(ProjetML1)
 
 data_test <- readRDS("data/imp_edges_test.rds")
-pred1 <- readRDS("data/naivePrediction1.rds")
-
-rmse(data_test[[1]]$rateCar - pred1[[1]])
 
 
+pred1 <- readRDS("data/naivePrediction1.rds") #5.844
+pred2 <- readRDS("data/naivePrediction2.rds") #5.899
+pred3 <- readRDS("data/naivePrediction3.rds") #3.955
 
+rmse_list <- c()
+for(i in 1:69){
+  rmse_list <- c(rmse_list, rmse(pred3[[i]], data_test[[i]]$rateCar))
+}
+mean(rmse_list)
 
-
+df_naiveModels <- data.frame(5.844, 5.899, 3.955)
+names(df_naiveModels) <- c("naiveModel1", "naiveModel2", "naiveModel3")
 
 
 
